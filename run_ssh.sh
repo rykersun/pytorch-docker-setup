6181871c43f825d9d9f22041afa9386293de2579
Ryker Sun <rykersun2002@gmail.com>
2023-01-30 11:40:26 +0800

    Update mount path

--- run_ssh.sh
+++ run_ssh.sh
@@ -1,2 +1,2 @@
-docker run -it --rm -v ./simultaneous_semantic_and_instance_segmentation:/root/simultaneous_semantic_and_instance_segmentation --name test -p 5678:22 -d detectron2
+docker run -it --rm -v $(pwd)/simultaneous_semantic_and_instance_segmentation:/root/simultaneous_semantic_and_instance_segmentation --name test -p 5678:22 -d detectron2
 docker inspect -f "{{ .NetworkSettings.IPAddress }}" test
